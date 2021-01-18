7c58eea56a8e5070c12f7c4b66eaa8bd581bd7c6
niraj baraskar <nirajbaraskar999@gmail.com>
2021-01-18 20:41:27 +0530

    initial commit

---
+++ src/robot.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Ledsack',
+        username: 'Samantha',
+        email: 'Nathan@yesensia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Mrs. dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info'
+    },
+    {
+        id: 6,
+        name: 'Kurtis weissnat',
+        username: 'Elvis_Presley',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 7,
+        name: 'Niraj Baraskar',
+        username: 'Niru',
+        email: 'niraj@baraskar.com'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Scherwood@rosanond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenn Reichard',
+        username: 'Dolphine',
+        email: 'Chain_MacCarthy.@io'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Mriah.Staton',
+        email: 'Rey.Padberg@karina.biz'
+    }
+]
